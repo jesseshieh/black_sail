e5ecc153ce22c8d0a1506d8ed723789b5665d563
reetou <fillmasterson@gmail.com>
2019-10-12 20:25:25 +0300

    Revert consumer supervisor startup config

--- apps/bot/lib/consumer_supervisor.ex
+++ apps/bot/lib/consumer_supervisor.ex
@@ -15,7 +15,9 @@ defmodule Bot.ConsumerSupervisor do
 
   @impl true
   def init(_args) do
-    children = [Supervisor.child_spec({Bot.Consumer, []}, id: {:bot, :consumer, 1})]
+    children =
+      for n <- 1..System.schedulers_online(),
+          do: Supervisor.child_spec({Bot.Consumer, []}, id: {:bot, :consumer, n})
     Supervisor.init(children, strategy: :one_for_one)
   end
 end
